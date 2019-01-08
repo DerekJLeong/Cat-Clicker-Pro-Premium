dee5eb25e80112e9b707c38df917cfce197bb170
Derek J Leong <derekjleong@gmail.com>
2019-01-08 12:42:43 -0800

    Added listView

--- js/app.js
+++ js/app.js
@@ -30,11 +30,42 @@ let data = {
     ]
 };
 
+
 // - - - - OCTO - - - - //
 
-// - - - - VIEW - - - - //
+let octo = {
+    init: function() {
+        //sets inital currentCat
+        data.currentCat = data.cats[0];
 
-let catDisplay = {
+        //iniialize views
+        listView.init();
+        catView.init();
+    },
+
+    getCurrentCat: function () {
+        return data.currentCat;
+    },
+
+    getCats: function() {
+        return data.cats;
+    },
+    
+    setCurrentCat: function(cat) {
+        data.currentCat = cat;
+    },
+
+    incrementCount: function () {
+        data.currentCat.clickCount++;
+        //render to display new count number
+        catView.render();
+    }
+};
+
+
+// - - - - VIEW - - - - //
+//View for displayed image and clickCount
+let catView = {
     init: function() {
         //Initialize and link variables with HTML elements
         this.displayCat = document.getElementById('cat-display');
@@ -44,7 +75,7 @@ let catDisplay = {
 
         //increment to currently displayed cats counter
         this.displayImg.addEventListener('click', function(){
-            octo.incrementCounter();
+            octo.incrementCount();
         });
 
         //render view
@@ -56,6 +87,47 @@ let catDisplay = {
         let currentCat = octo.getCurrentCat();
         this.displayName.textContent = currentCat.name;
         this.displayCount.textContent = currentCat.clickCount;
-        this.displayName.src = currentCat.imgSrc;
+        this.displayImg.src = currentCat.imgSrc;
     }
 };
+
+//View for list of cats to select from
+let listView = {
+    init: function() {
+        //inialize and link with the HTML elem
+        this.displayList = document.getElementById('cat-list');
+
+        this.render();
+    },
+
+    render: function() {
+        let cat, catElem, i;
+
+        let cats = octo.getCats();
+
+        //Clears list before loop
+        this.displayList.innerHTML = '';
+
+        for (i = 0; i < cats.length; i++) {
+            //cat currently being looped over
+            cat = cats[i];
+
+            //create a new list item and set its text
+            catElem = document.createElement('li');
+            catElem.textContent = cat.name;
+
+            //on click will update the currentCat and re run the catView render
+            catElem.addEventListener('click', (function(copyCat) {
+                return function() {
+                    octo.setCurrentCat(copyCat);
+                    catView.render();
+                }
+            })(cat));
+
+            this.displayList.appendChild(catElem);
+        }
+    }
+};
+
+//Starts App
+octo.init();
